10a6aad74beddd648c868999675128eb8c67521e
Hlushchuk Dzmitry <glushhukd@gmail.com>
2024-06-20 12:39:29 +0300

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Dzmitry Hlushchuk on 20.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
